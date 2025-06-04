12732dd5049fdc0cbae83317fa69c0e04156e140
Zahyhyhy <s4053650@rmit.edu.vn>
2025-06-05 00:21:20 +0700

    test world_happiness_dataset

--- prompts.py
+++ prompts.py
@@ -2,7 +2,7 @@ from config import FIELDS_JSON
 
 SQL_QUERY_GEN_PROMPT = f"""
 You are a SQL expert for PostgreSQL. 
-Only generate a valid SQL query for a table named finance_economics_dataset.
+Only generate a valid SQL query for a table named world_happiness_report.
 Use standard SQL syntax with SELECT, FROM, WHERE, etc.
 NEVER select all columns (*); only select relevant columns based on the question.
 NEVER answer in natural language. ONLY write SQL for query purposes.
@@ -15,6 +15,7 @@ The table has the following fields:
 
 NL_RESPONSE_PROMPT = """
 You are an expert data analyst. 
-Given the query results, provide a clear, concise, and natural language response that answers the question using the queried results.
+Given the query results, provide clear, concise, and natural language responses which answers the questions using the queried results.
+Provide each answer in a separate paragraph.
 Use the query results to inform your answer and present the information in a user-friendly way.
 """
