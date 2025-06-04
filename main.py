4d009d99ad189310d63cffb0805def332509e450
Zahyhyhy <s4053650@rmit.edu.vn>
2025-06-05 00:21:20 +0700

    test world_happiness_dataset

--- main.py
+++ main.py
@@ -8,24 +8,31 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 def main():
-    user_query = "Calculate and come up with a trading strategy for gold and stock, provide clear analysis"
+    user_queries = [
+        "Which countries had the highest life ladder scores in 2020?",
+        "Compare the average life ladder score between Western Europe and Sub-Saharan Africa in 2021. Which region had the higher score?",
+        "Which countries in Latin America had above-average social support and also experienced below-average negative affect in 2020?",
+        "Which countries showed a consistent increase in confidence in national government every year from 2018 to 2021?",
+        "Find countries where positive affect was above 0.7 and negative affect was below 0.3 in 2021.",
+        "What are the top 3 countries with the highest freedom to make life choices among those with a life ladder score below 5 in 2019?"
+    ]
 
     conn = get_db_connection()
     if conn is None:
         logger.error("Please check your PostgreSQL configuration. Exiting...")
         sys.exit(1)
     if not check_table_exists(conn):
-        logger.error("Table 'finance_economics_dataset' does not exist. Please create the table and load the data. Exiting...")
+        logger.error("Table 'world_happiness_report' does not exist. Please create the table and load the data. Exiting...")
         conn.close()
         sys.exit(1)
     conn.close()
 
-    if not user_query:
+    if not user_queries:
         logger.error("No query provided. Please set a valid query in the code.")
         return
 
     state = {
-        'question': user_query,
+        'question': user_queries,
         'query': '',
         'query_result': '',
         'final_answer': ''
