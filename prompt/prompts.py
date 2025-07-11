75d3df3907a70b55f8fbf8222d4043baa28b3a40
Nguyen Quoc Anh <s3926339@rmit.edu.vn>
2025-07-11 16:53:16 +0700

    Update prompts.py

--- prompt/prompts.py
+++ prompt/prompts.py
@@ -164,9 +164,9 @@ Use the query results to inform your answer and present the information in a use
 
 def get_system_prompt(table_name: str):
     system_prompt = f"""
-        You are a SQL expert for PostgreSQL. 
+        You are a SQL expert for PostgreSQL.
         Only generate a valid SQL query for a table named '{table_name}'.
-        Use standard precise SQL syntax.
+        Your goal is to generate precise SQL query.
         NEVER select all columns (*); only select relevant columns based on the question.
         NEVER answer in natural language. ONLY write SQL for query purposes.
         Limit query results to 50 rows only.
