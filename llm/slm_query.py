b881fbb3391e8b1ddecb39849d8e11f42b923e4c
Nguyen Vu Thien Ngan <thienngan266@gmail.com>
2025-08-06 14:32:22 +0700

    run finance_economics dataset and metrics

--- llm/slm_query.py
+++ llm/slm_query.py
@@ -22,6 +22,9 @@ logger = logging.getLogger(__name__)
 def get_data_fields_meaning(table_name: str) -> str:
     if table_name not in DATA_FIELDS_MEANING:
         raise ValueError(f"Table '{table_name}' non-existent in DATA_FIELDS_MEANING.")
+    
+
+
     fields = DATA_FIELDS_MEANING[table_name]['fields']
     lines = [f"{k}: {v}" for k, v in fields.items()]
     return "\n".join(lines)
@@ -47,11 +50,11 @@ models = [
         'qwen3:4b',    
     ]
 
-TABLE_NAME = "global_development_indicators"
+TABLE_NAME = "finance_economics"
 
 OUTPUT_DIR = os.path.join(
      os.path.dirname(__file__),
-     "..", "query", "output", "falcon3", "global_development_indicators", "pred_sql"
+     "..", "query", "output", "cogito:3b", "finance_economics", "pred_sql"
  )
     
 # === REMOVE NEWLINES IN QUERY ===
@@ -124,37 +127,37 @@ def save_ground_truth(state: State, idx, gen_time=None, output_dir=OUTPUT_DIR):
 # === MAIN PIPELINE ===
 if __name__ == "__main__":
     user_queries = [
-    "What is the digital readiness score of South Asia in 2020?",
-    "Country with the highest global development resilience score in 2020",
-    "What is the human development index score of Vietnam in 2019?",
-    "What is the inflation rate of Vietnam in 2009?",
-    "What is the unemployment rate of Europe region in 2008?",
-    "What is the life expectancy of Japan in 2020?",
-    "What is the economic opportunity index of indebt countries group in 2020?",
-    "What is the gpd per capita of OECD group in 2019?",
-    "What is the green transition score of lower middle income group in 2020?",
-    "What is the unemployment rate of US in 2020?",
-    "To what extent has the world's GDP per capita grown between 2000 and 2020?",
-    "What is the average economic opportunity index of low and indebt countries?",
-    "How many countries has the gdp per capita lower than the world in 2019?",
-    "Which country has the highest health expenditure per capita in 2020?",
-    "How many countries are in lower middle income group as in 2020?",
-    "What is the average digital readiness score of high income countries in 2019?",
-    "Which country has the lowest global resilience score?",
-    "What is the highest energy per capita score in 2020? In which country",
-    "What country has the longest period of deflation? Deflation is known as when the inflation is negative",
-    "Which country has the highest gdp per capita among lower middle income group?",
-    "Number of countries have higher economic opportunity than the world",
-    "Looking at the green transition score of all the countries and regions over the years, what is the common patterns?",
-    "Explain for me the correlations of human development index and gdp per capita",
-    "Evaluate the differences in digital readiness of South Asia compared to Europe region",
-    "Is there any correlation between governance quality and digital readiness score of South Asia?",
-    "Does the school enrolment in secondary affect the HDI in South Asia?",
-    "What is the relationship of the FDI percentage of gdp and economic grow in middle and low income group?",
-    "Compare the economic opportunity index of high income, middlie income groups and the world",
-    "Is the weather vulnerability related to the child mortality in high income country?",
-    "Do countries with high weather vulnerability score have higher green transition scores? Explain for me the implication",
-    "What is the relationship between unemployment rate and inflation of US through years?"
+    "The inflation rate of November 28, 2008",
+    "The open price, close price, trading volume of Feb 20, 2000",
+    "The gold price, crude oil price, real estate index of Jan 1, 2008",
+    "The consumer confidence index of 24 December, 2007",
+    "The date, stock index on which stock index reached the highest Daily High",
+    "The date, gold price, crude oil price and gdp growth where unemployment rate reached the highest",
+    "The date, forex US/EUR, US/JPY where gdp growth is negative",
+    "The date where trading volumne is the largest",
+    "The date, consumer confidence index, consumer spending where the bankruptcy rate is highest",
+    "The unemployment rate, interest rate, inflation rate, the date where Dow Jones has top trading volume",
+    "Number of days which the closing price is higher than the open price of Dow Jones, S&P 500, NASDAQ",
+    "The annual consumer spending from 2000 to 2008",
+    "The annual growth rate of consumer confidence index from 2000 to 2008",
+    "Ratio of days having negative gdp growth to total days",
+    "The average bankruptcy rate and M&A deals by year",
+    "Number of days where the gdp growth is negative",
+    "Average monthly trading volume of NASDAQ, S&P 500, Dow Jones",
+    "Which year has the highest average consumer confidence index?",
+    "The annual average venture capital funding by year",
+    "The ratio of government debt to consumer spending by year",
+    "Average gold price by year",
+    "2008 was the year of global financial crisis. Evaluate some indicators that signaled this recession, and propose a risk mitigation strategy for stock investing",
+    "Does government debt effect GDP growth? Analyze the given data and recommend policymakers on how to create positive gdp growth",
+    "Low interest rate leads to risky money flow into stock and consumer spending. Evaluate the data to verify the accuracy of this statement. ",
+    "Analyze the past performance of all the asset types, stock, commodity, real estate, forex and propose which type of asset is worth to invest in based on investors' risk apetite",
+    "Evaluate the relationship between inflation rate and consumer spending",
+    "Which stock indexes should investors invest in? Analyze NASDAQ, S&P 500, Dow Jones performances",
+    "What is the relationship between interest rate, unemployment rate and corporate profits? Propose strategy for policymakers to promote corporate activities",
+    "venture capital funding is private fundraising, wheras stock index activities is public fundraising. Which fundraising channel is more active in US, explain the reasons",
+    "Recommend asset classes to invest when the USD currency is appreciated, and when it is depreciated",
+    "When it is best to invest in real estate in terms of interest rate environment, stock performance and unemployment context?"
     ]
     if not user_queries:
         logger.error("No query provided. Please set a valid query in the code.")
@@ -174,8 +177,8 @@ if __name__ == "__main__":
                 end_time = time.time()
                 gen_time = end_time - start_time
                 local_tablename = TABLE_NAME
-                if local_tablename == 'global_development_indicators':
-                    local_tablename = 'global_development'
+                if local_tablename == 'finance_economics':
+                    local_tablename = 'finance_economics'
                 model_output = os.path.join(
                     os.path.dirname(__file__),
                     "..", "query", "output", model, local_tablename, "pred_sql"
