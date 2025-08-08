26076d9f55fa53c6c9d2aed58910e050f054b94a
Nguyen Vu Thien Ngan <thienngan266@gmail.com>
2025-08-08 23:14:02 +0700

    output of happiness_record

--- llm/slm_query.py
+++ llm/slm_query.py
@@ -38,23 +38,23 @@ class State(TypedDict):
 
 # === CONFIGURATION ===
 models = [
-        'cogito:3b',
-        'deepseek-r1:7b',
-        'gemma3:4b',
-        'gemma3n:e4b',
-        'llama3.2:latest',
-        'mistral:7b',
+        # 'cogito:3b',
+        # 'deepseek-r1:7b',
+        # 'gemma3:4b',
+        # 'gemma3n:e4b',
+        # 'llama3.2:latest',
+        # 'mistral:7b',
         'phi3.5:3.8b',
         'phi4-mini:3.8b',
         'qwen2.5:3b',
         'qwen3:4b',    
     ]
 
-TABLE_NAME = "finance_economics"
+TABLE_NAME = "world_happiness_report"
 
 OUTPUT_DIR = os.path.join(
      os.path.dirname(__file__),
-     "..", "query", "output", "cogito:3b", "finance_economics", "pred_sql"
+     "..", "query", "output", "cogito:3b", "world_happiness_report", "pred_sql"
  )
     
 # === REMOVE NEWLINES IN QUERY ===
@@ -127,37 +127,37 @@ def save_ground_truth(state: State, idx, gen_time=None, output_dir=OUTPUT_DIR):
 # === MAIN PIPELINE ===
 if __name__ == "__main__":
     user_queries = [
-    "The inflation rate of November 28, 2008",
-    "The open price, close price, trading volume of Feb 20, 2000",
-    "The gold price, crude oil price, real estate index of Jan 1, 2008",
-    "The consumer confidence index of 24 December, 2007",
-    "The date, stock index on which stock index reached the highest Daily High",
-    "The date, gold price, crude oil price and gdp growth where unemployment rate reached the highest",
-    "The date, forex US/EUR, US/JPY where gdp growth is negative",
-    "The date where trading volumne is the largest",
-    "The date, consumer confidence index, consumer spending where the bankruptcy rate is highest",
-    "The unemployment rate, interest rate, inflation rate, the date where Dow Jones has top trading volume",
-    "Number of days which the closing price is higher than the open price of Dow Jones, S&P 500, NASDAQ",
-    "The annual consumer spending from 2000 to 2008",
-    "The annual growth rate of consumer confidence index from 2000 to 2008",
-    "Ratio of days having negative gdp growth to total days",
-    "The average bankruptcy rate and M&A deals by year",
-    "Number of days where the gdp growth is negative",
-    "Average monthly trading volume of NASDAQ, S&P 500, Dow Jones",
-    "Which year has the highest average consumer confidence index?",
-    "The annual average venture capital funding by year",
-    "The ratio of government debt to consumer spending by year",
-    "Average gold price by year",
-    "2008 was the year of global financial crisis. Evaluate some indicators that signaled this recession, and propose a risk mitigation strategy for stock investing",
-    "Does government debt effect GDP growth? Analyze the given data and recommend policymakers on how to create positive gdp growth",
-    "Low interest rate leads to risky money flow into stock and consumer spending. Evaluate the data to verify the accuracy of this statement. ",
-    "Analyze the past performance of all the asset types, stock, commodity, real estate, forex and propose which type of asset is worth to invest in based on investors' risk apetite",
-    "Evaluate the relationship between inflation rate and consumer spending",
-    "Which stock indexes should investors invest in? Analyze NASDAQ, S&P 500, Dow Jones performances",
-    "What is the relationship between interest rate, unemployment rate and corporate profits? Propose strategy for policymakers to promote corporate activities",
-    "venture capital funding is private fundraising, wheras stock index activities is public fundraising. Which fundraising channel is more active in US, explain the reasons",
-    "Recommend asset classes to invest when the USD currency is appreciated, and when it is depreciated",
-    "When it is best to invest in real estate in terms of interest rate environment, stock performance and unemployment context?"
+   "What is the positive affect of US in 2008?",
+    "What is the life ladder of Vietnam in 2022?",
+    "What is the long GDP per capita of China in 2009?",
+    "What is the social support index of Germany in 2008?",
+    "What is the Healthy Life Expectancy At Birth of Japan in 2020?",
+    "What is the Freedom To Make Life Choices of Vietnam in 2018?",
+    "What is the generiosity score of Singapore in 2022?",
+    "What is the Perceptions Of Corruption of Ecuador in 2008?",
+    "What is the negative affect of Japan in 2022?",
+    "What is the Confidence In National Government of Singapore in 2022?",
+    "What is the average positive affect of all countries in 2008?",
+    "What is the average life ladder of Asia countries in 2022?",
+    "Which country has the highest average long gdp per capita through years?",
+    "Which country has the lowest average social support score over years?",
+    "What is the average Healthy Life Expectancy At Birth of the world after 2020? ",
+    "Which country has the highest freedom to make choices index? What region is it from? ",
+    "How many countries have higher average generiosity over years compared to the world' average?",
+    "Which region has the highest average perceptions of corruption?",
+    "Which country has the highest average negative affect in 2022? ",
+    "What is the average Confidence In National Government score of Vietnam over years?",
+    "What are the countries having confidence in national goverment higher than world average in 2022?",
+    "Interpret for me the current trend in positive affect of the world from 2008 to 2022?",
+    "Comparing the life ladder of Asia and Europe to tell me which region is worth to live?",
+    "Are countries with higher long gdp per capita likely to have higher positive effect over years? Comparing the data to evaluate ",
+    "Does lack of social support lead to higher negative affect over years? ",
+    "Analyze the trend of Healthy Life Expectancy At Birth by region (Asia, Europe, North America)",
+    "Evaluate for me the relationship between freedom to make choices and positive affect of Asia and North America countries. Explain the reasons.",
+    "What is the current movement of generiosity of Asian countries over years? Explain the reasons.",
+    "What is the relationship of percptions of corruption and confidence in national government?",
+    "Do high long gdp per capita countries have higher negative affects? Explain the results.",
+    "How differences in Confidence In National Government score comparing all the regions? Evaluate the importance of governance trust on positive affects."
     ]
     if not user_queries:
         logger.error("No query provided. Please set a valid query in the code.")
@@ -177,8 +177,8 @@ if __name__ == "__main__":
                 end_time = time.time()
                 gen_time = end_time - start_time
                 local_tablename = TABLE_NAME
-                if local_tablename == 'finance_economics':
-                    local_tablename = 'finance_economics'
+                if TABLE_NAME == 'world_happiness_report':
+                    local_tablename = 'happiness_record'
                 model_output = os.path.join(
                     os.path.dirname(__file__),
                     "..", "query", "output", model, local_tablename, "pred_sql"
